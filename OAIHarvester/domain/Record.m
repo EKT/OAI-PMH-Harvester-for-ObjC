83aae80c9477f007159bd08794c9c7feec94b5a6
Kostas Stamatis <kstamatis@ekt.gr>
2015-10-02 23:27:45 +0300

    fix

--- OAIHarvester/domain/Record.m
+++ OAIHarvester/domain/Record.m
@@ -16,11 +16,11 @@
 - (id) initWithXMLElement:(CXMLElement *)recordXMLElement{
     if (self = [super init]){
         CXMLElement *headerElement = nil;
-        NSArray *headerArray = [recordXMLElement elementsForLocalName:@"header" URI:BASE_NAMESPACE];
+        NSArray *headerArray = [recordXMLElement elementsForName:@"header"];
         if ([headerArray count]>0)
             headerElement = [headerArray objectAtIndex:0];
         CXMLElement *metadataElement = nil;
-        NSArray *metadataArray = [recordXMLElement elementsForLocalName:@"metadata" URI:BASE_NAMESPACE];
+        NSArray *metadataArray = [recordXMLElement elementsForName:@"metadata"];
         if ([metadataArray count]>0)
             metadataElement = [metadataArray objectAtIndex:0];
         self.recordMetadata = [[RecordMetadata alloc] initWithXMLElement:metadataElement];
